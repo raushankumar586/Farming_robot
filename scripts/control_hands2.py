4bb9c6780bd420e5a123d504eeca3709f41c39d5
Raushan Kumar <raushank@invento.tech>
2020-07-13 22:11:56 +0530

    latest

--- scripts/control_hands2.py
+++ scripts/control_hands2.py
@@ -37,23 +37,23 @@ right_down = rospy.Publisher(
 
 hand  = 1.0 
 
-def position_control(j1=0,j2=0,j3=0,j4=0,j5=0,j11=0,j12=0,j13=0,j14=0,j15=0, lt=0, ld=0,rt=0,rd=0):
+def position_control(j1=0,j2=0,j3=0,j4=0,j5=0,lt=0, ld=0):
     
     joint1_pub.publish(j1)
     joint2_pub.publish(j2)
     joint3_pub.publish(j3)
     joint4_pub.publish(j4)
     joint5_pub.publish(j5)
-    joint11_pub.publish(j11)
-    joint12_pub.publish(j12)
-    joint13_pub.publish(j13)
-    joint14_pub.publish(j14)
-    joint15_pub.publish(j15)
     left_top.publish(lt)
     left_down.publish(ld)
-    right_top.publish(rt)
-    right_down.publish(rd)
     
+
+def gripper_control(data):
+    left_top.publish(data.data)
+    left_down.publish(data.data)
+    rospy.logwarn("gripper  data {}".format(data.data))
+    
+
 
 def position_control_left(j11=0,j12=0,j13=0,j14=0,j15=0):
     joint11_pub.publish(j11)
@@ -69,25 +69,6 @@ def position_control_right(j1=0,j2=0,j3=0,j4=0,j5=0):
     joint3_pub.publish(j3)
     joint4_pub.publish(j4)
     joint5_pub.publish(j5)
-# def main():
-#     rospy.init_node("control_hands", anonymous=True)
-#     global rate
-#     rate = rospy.Rate(5)
-    
-    # while not rospy.is_shutdown():
-    #     for i in range(20):
-            
-            
-    #         position_control(0,3,6,-4,0 -0,-1,4,-10,-20)
-    #         rate.sleep() 
-    #         print ('Completed first round')
-
-    #     for i in range(20):
-            
-    #         position_control()
-    #         rate.sleep() 
-    #         print ('Full rotation completed')
-
 
 def which_hand(hands):
    hand= hands.data
@@ -106,16 +87,16 @@ def callback(GST):
         elif hand == 2.0 :
             position_control_right(0.36,0.93,0.65,0.65,0.33)
         else :
-            position_control(0.36,0.93,0.65,0.65,0.33, 0.36,-0.93,-0.65,-0.65,-0.33)
+            position_control(0.36,0.93,0.65,0.65,0.33)
     elif Gesture == 2.0 :
          position_control()
          
-    elif Gesture == 3.0 :
-        position_control(-0.10,1.57, 0.0,1.57,0.25, 0.10,-1.57,-0.0,-1.57,0.25,5.0,5.0,5.0,5.0)
-    elif Gesture == 4.0 :
-        position_control(-1.46,1.57,0.04,0.0,-0.42, 1.55,0.0,-1.57,0.0,0.0)
-    elif Gesture == 5.0 :
-        position_control(0,0.1, 0.2,0.3,0.4, -0,-0.1, -0.2,-0.3,-0.4)
+    elif Gesture == 3.0:
+        position_control(-0.10, 1.57, 0.0, 1.57, 0.25, 5.0, 5.0)
+    elif Gesture == 4.0:
+        position_control(0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 3.0)
+    elif Gesture == 5.0:
+        position_control(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
     else:
         print('Exited')
     pass
@@ -127,8 +108,9 @@ def subscriber():
     rospy.init_node("control_hands", anonymous=True)
     global rate
     rate = rospy.Rate(10)
-    rospy.Subscriber("/both_hands",Float64,callback)
+    rospy.Subscriber("/right_hands",Float64,callback)
     rospy.Subscriber("/single_hand",Float64, which_hand)
+    rospy.Subscriber("/gripper",Float64, gripper_control)
     print('After subscribe')
     rospy.spin()
 
